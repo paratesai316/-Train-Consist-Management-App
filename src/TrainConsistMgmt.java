2d48c7968947c7281b81fb650ce8a4fa29074160
Sai Parate <paratesai316@gmail.com>
2026-04-13 09:47:36 +0530

    UC6

--- src/TrainConsistMgmt.java
+++ src/TrainConsistMgmt.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrainConsistMgmt {
+
     public static void main(String[] args) {
 
+        HashMap<String, Integer> bogieCapacityMap = new HashMap<>();
+
+        bogieCapacityMap.put("Sleeper", 72);
+        bogieCapacityMap.put("AC Chair", 60);
+        bogieCapacityMap.put("First Class", 24);
+
+        System.out.println("Bogie Capacity Mapping:");
+
+        for (Map.Entry<String, Integer> entry : bogieCapacityMap.entrySet()) {
+            System.out.println("Bogie Type: " + entry.getKey() + " | Capacity: " + entry.getValue());
+        }
     }
 }
