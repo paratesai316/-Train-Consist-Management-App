3260d47242c1ac66ca84f3a912b4e6062412d645
Sai Parate <paratesai316@gmail.com>
2026-04-13 09:46:31 +0530

    UC5

--- src/TrainConsistMgmt.java
+++ src/TrainConsistMgmt.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedHashSet;
+
 public class TrainConsistMgmt {
+
     public static void main(String[] args) {
 
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+
+        System.out.println("Initial formation: " + trainFormation);
+
+        boolean isAdded = trainFormation.add("Sleeper");
+
+        if (!isAdded) {
+            System.out.println("Attempted to add duplicate 'Sleeper'. System rejected it.");
+        }
+
+        System.out.println("Final Train Formation: " + trainFormation);
     }
 }
