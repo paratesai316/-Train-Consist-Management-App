bc354a44ae192fd1801d79264e48e3991b844fb4
Sai Parate <paratesai316@gmail.com>
2026-04-13 09:43:45 +0530

    UC2

--- src/TrainConsistMgmt.java
+++ src/TrainConsistMgmt.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class TrainConsistMgmt {
+
     public static void main(String[] args) {
 
+        // 1. Create an ArrayList<String> for passenger bogies
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+        // 2. Add bogies: Sleeper, AC Chair, First Class
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        // 3. Print the list after insertion
+        System.out.println("Initial Passenger Bogies: " + passengerBogies);
+
+        // 4. Remove one bogie (AC Chair)
+        passengerBogies.remove("AC Chair");
+        System.out.println("Bogie removed. Updated List: " + passengerBogies);
+
+        // 5. Use contains() to check if Sleeper exists
+        boolean hasSleeper = passengerBogies.contains("Sleeper");
+        System.out.println("Does consist contain 'Sleeper'? " + hasSleeper);
+
+        // 6. Print final list state
+        System.out.println("Final list state: " + passengerBogies);
     }
 }
